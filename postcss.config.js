0fd43ce8d813b8a65c66b2d6204a0bb8baee0857
root <root@Alba>
2024-07-25 16:29:25 +0200

    modif

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,16 @@
 
+<<<<<<< HEAD
 export default {
     plugins: {
         tailwindcss: {},
         autoprefixer: {},
     },
 };
+=======
+module.exports = {
+    plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
+    ],
+}
+>>>>>>> 6e03920 (gitignore-config)
