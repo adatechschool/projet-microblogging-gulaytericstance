c9e0247d21b87abfed0f937e795c11f4ef6e73fd
constance <constance.mendez.harscouet@gmail.com>
2024-07-25 16:21:28 +0200

    ajout fichiers gitignore

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 module.exports = {
     plugins: [
         require('tailwindcss'),
         require('autoprefixer'),
     ],
 }
-=======
-export default {
-    plugins: {
-        tailwindcss: {},
-        autoprefixer: {},
-    },
-};
->>>>>>> 5e00f9a (breeze)
