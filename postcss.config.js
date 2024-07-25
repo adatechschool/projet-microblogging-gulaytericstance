71ecf474859a0bcb7cb3da418d10d6d33b265907
root <root@Alba>
2024-07-25 10:57:05 +0200

    modif1

--- postcss.config.js
+++ postcss.config.js
@@ -12,4 +12,8 @@ export default {
         autoprefixer: {},
     },
 };
+<<<<<<< HEAD
 >>>>>>> 5e00f9a (breeze)
+=======
+>>>>>>> 13b0932 (modif1)
+>>>>>>> a5c818d (modif1)
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+use App\Http\Controllers\ProfileController;
+=======
+>>>>>>> 13b0932 (modif1)
+>>>>>>> a5c818d (modif1)
 use Illuminate\Support\Facades\Route;
 
 /*
