5908a3eb5c0a2d456c76d818b3a96484ab8eff37
root <root@Alba>
2024-07-25 16:04:49 +0200

    solve conflicts
    git push origin main

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
-
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+<<<<<<< HEAD
 use App\Http\Controllers\PostController;
 
+=======
+>>>>>>> a804d73 (solve conflicts)
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,20 +16,26 @@ use App\Http\Controllers\PostController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
 Route::get('/', function () {
     return view('welcome');
 });
+<<<<<<< HEAD
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+=======
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+>>>>>>> a804d73 (solve conflicts)
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+<<<<<<< HEAD
 
 
 
@@ -39,4 +47,6 @@ Route::middleware('auth')->group(function () {
     return view('home');
 });
 
+=======
+>>>>>>> a804d73 (solve conflicts)
 require __DIR__.'/auth.php';
