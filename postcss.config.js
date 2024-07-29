029ee9aa5103ff7b4f70f3a571cca607a70d1db9
root <root@Alba>
2024-07-29 09:33:23 +0200

    Modifications apportées aux fichiers docker-compose.yml et postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,9 @@
 
-<<<<<<< HEAD
+
 export default {
     plugins: {
         tailwindcss: {},
         autoprefixer: {},
     },
 };
-=======
-module.exports = {
-    plugins: [
-        require('tailwindcss'),
-        require('autoprefixer'),
-    ],
-}
->>>>>>> 6e03920 (gitignore-config)
+
