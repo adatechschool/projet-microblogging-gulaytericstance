89fe77ef39fcea6611ab607d3a4839868f00c431
gugu57 <gulbahar57@icloud.com>
2024-07-29 15:39:14 +0200

    resolve conflict on web php

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,8 @@
 <?php
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
 use App\Http\Controllers\PostController;
 
-=======
->>>>>>> a804d73 (solve conflicts)
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -19,23 +16,14 @@ use App\Http\Controllers\PostController;
 Route::get('/', function () {
     return view('welcome');
 });
-<<<<<<< HEAD
-
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
-
-=======
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
->>>>>>> a804d73 (solve conflicts)
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-<<<<<<< HEAD
 
 
 
@@ -47,6 +35,4 @@ Route::middleware('auth')->group(function () {
     return view('home');
 });
 
-=======
->>>>>>> a804d73 (solve conflicts)
 require __DIR__.'/auth.php';
