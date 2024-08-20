627301c6c2c5dc09400209fcddc88ad2ca59f13c
constance <constance.mendez.harscouet@gmail.com>
2024-08-20 17:13:32 +0200

    test user is not registered a revoircar il ne renvoie rien

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,18 +2,52 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 // use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
+
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
-    public function test_the_application_returns_a_successful_response(): void
+        public function test_user_is_not_registered($username): void
+        {
+            $user = User::where('name', $username)->first();
+           
+            if (!$user) {
+                $this->fail("L'utilisateur $username n'est pas enregistré.");
+            }
+
+            
+        }
+    
+        public function test_users_are_not_registered(): void
+        {
+            $this->test_user_is_not_registered('Enola Nolan'); 
+           
+        }
+}
+
+         /*
+     public function test_the_application_returns_a_successful_response(): void
     {
         $response = $this->get('/');
 
         $response->assertStatus(200);
     }
-}
+     */
+    
+
+    /*public function test_user_not_registered(): void
+    {
+        $this -> assertDatabaseMissing('users', [
+            'email' => 'nonexistent@example.com'
+        ]);   
+    }
+        */
+
+    /*public function test_invalid_email():void
+    {
+        $this -> assertDoesNotMatchRegularExpression('email', [
+            'email' =>
+        ])
+    }*/
